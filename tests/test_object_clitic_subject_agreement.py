881facb28b76a0bf0f9b0633e4b1f6aef79585b4
bardooran <bardooran@gmail.com>
2026-08-30 12:39:57 +0200

    Test arag role and aspect evidence

--- tests/test_object_clitic_subject_agreement.py
+++ tests/test_object_clitic_subject_agreement.py
@@ -18,7 +18,7 @@ def test_rule_ids_are_unique_and_native_reviewed():
 
 def test_idin_is_object_not_agreement_controller():
     by_id = {rule["id"]: rule for rule in load_rules()}
-    for rule_id in ["GRAM-OBJAGR-001", "GRAM-OBJAGR-002", "GRAM-OBJAGR-003", "GRAM-OBJAGR-004"]:
+    for rule_id in ["GRAM-OBJAGR-001", "GRAM-OBJAGR-002", "GRAM-OBJAGR-003", "GRAM-OBJAGR-004", "GRAM-OBJAGR-006"]:
         rule = by_id[rule_id]
         assert rule["object_clitic"] == "idin"
         assert rule["verb_agreement_controller"] in {"understood_subject", "subject"}
@@ -33,6 +33,17 @@ def test_masculine_and_feminine_lion_agreement_stays_distinct():
     assert masc["subject_gender"] == "masculine"
     assert fem["question_example"] == "Libaaxadu maydin eryanaysaa?"
     assert fem["subject_gender"] == "feminine"
+
+
+def test_bare_maydin_cun_gender_contrast_is_preserved():
+    by_id = {rule["id"]: rule for rule in load_rules()}
+    fem = by_id["GRAM-OBJAGR-002"]
+    masc = by_id["GRAM-OBJAGR-006"]
+    assert fem["example"] == "Maydin cunaysaa?"
+    assert fem["subject_gender"] == "feminine"
+    assert masc["example"] == "Maydin cunayaa?"
+    assert masc["subject_gender"] == "masculine"
+    assert fem["object_clitic"] == masc["object_clitic"] == "idin"
 
 
 def test_definite_and_subject_marked_noun_forms_are_not_collapsed():
@@ -56,6 +67,34 @@ def test_general_and_ongoing_cun_forms_stay_separate():
     assert by_id["GRAM-OBJAGR-001"]["aspect"] == "general_or_habitual"
     assert by_id["GRAM-OBJAGR-002"]["example"] == "Maydin cunaysaa?"
     assert by_id["GRAM-OBJAGR-002"]["aspect"] == "ongoing"
+
+
+def test_arag_first_person_subject_forms_are_both_preserved():
+    rule = {rule["id"]: rule for rule in load_rules()}["GRAM-OBJAGR-007"]
+    assert rule["subject_person"] == 1
+    assert rule["object_clitic"] == "idin"
+    assert "Maydin arkaa?" in rule["examples"]
+    assert "Maydin arkayaa?" in rule["examples"]
+
+
+def test_arag_role_contrasts_remain_separate():
+    by_id = {rule["id"]: rule for rule in load_rules()}
+    direct = by_id["GRAM-OBJAGR-008"]
+    reciprocal = by_id["GRAM-OBJAGR-009"]
+    impersonal = by_id["GRAM-OBJAGR-010"]
+
+    assert direct["example"] == "Maad i aragtaan?"
+    assert direct["subject_person"] == 2
+    assert direct["subject_number"] == "plural"
+    assert direct["object_clitic"] == "i"
+
+    assert reciprocal["example"] == "Ma is arkaysaan?"
+    assert reciprocal["marker"] == "is"
+
+    assert "Ma la idin arkaa?" in impersonal["examples"]
+    assert "Ma la idin arki karaa?" in impersonal["examples"]
+    assert impersonal["impersonal_marker"] == "la"
+    assert impersonal["object_clitic"] == "idin"
 
 
 def test_native_review_rules_do_not_define_string_replacements():
